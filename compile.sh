d0689aafa21eb5b79e4cbc1444eb9bf3c325031b
Kirinel <ycqstrong@icloud.com>
2019-05-31 13:53:26 -0500

    Add files via upload

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCIVIS=/Users/yanchuanqi/UChicago/Winter_2019/Scientific_Visualization/scivis-work/scivis-2019
+
+# cmd="cc -O2 -W -Wall -DTEEM_32BIT=1  -Wl,-prebind \
+#   -I$SCIVIS/tpz/include -o conegen conegen.c \
+#   -L$/Users/yanchuanqi/UChicago/Spring_2019/vpdata/teem-src/arch/darwi.64 -lteem \
+#   -lz -lpthread -lm"
+cmd="cc -g -Wall -O2 -o untext mean_stdv.c -I$SCIVIS/tpz/include -L$SCIVIS/tpz/lib-osx -ltpz -lm"
+echo $cmd
+eval $cmd
